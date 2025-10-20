0e49e5de2f39cd49d263cb1a73b69c420f65d874
mwiacek <maciejwiacek4@gmail.com>
2025-10-20 13:34:18 +0200

    Added basic user flow layout + updated gitignore

---
+++ app/(app)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Authenticated Flow</Text>
+    </View>
+  )
+}
+
+export default Index
---
+++ app/(auth)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>Authentication Flow</Text>
+    </View>
+  )
+}
+
+export default Index
