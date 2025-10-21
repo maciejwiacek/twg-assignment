f29e163bc3ff5f7896ae0ee169e96cc32ebbd154
mwiacek <maciejwiacek4@gmail.com>
2025-10-21 16:57:42 +0200

    Created prebuild and added React Native Video package

--- app/(app)/video/[videoId].tsx
+++ app/(app)/video/[videoId].tsx
@@ -3,12 +3,17 @@ import React from 'react'
 import { router, useLocalSearchParams } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useVideoDetails } from '@/hooks/useVideoDetails'
+import Video from 'react-native-video'
 
 const VideoDetails = () => {
   const { videoId } = useLocalSearchParams<{ videoId: string }>()
   const { data, isLoading, isError } = useVideoDetails(videoId)
   return (
-    <SafeAreaView>
+    <View>
+      <Video
+        source={require('@/assets/video/broadchurch.mp4')}
+        style={{ width: '100%', height: 280 }}
+      />
       <Text>{data?.snippet.title}</Text>
       <Text>{data?.snippet.description}</Text>
       <Text>{data?.snippet.publishedAt}</Text>
@@ -16,7 +21,7 @@ const VideoDetails = () => {
       <Text>{data?.statistics.viewCount}</Text>
       <Text>{data?.statistics.likeCount}</Text>
       <Button title='Go back' onPress={() => router.back()} />
-    </SafeAreaView>
+    </View>
   )
 }
 
